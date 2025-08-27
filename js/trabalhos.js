9863390c760c49545747ddfcf2103a04d396d3c3
Celeste Moraes <celestemoraes1706@gmail.com>
2025-08-26 22:01:34 -0300

    Alterações com json rodando

--- js/trabalhos.js
+++ js/trabalhos.js
@@ -1,20 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
 
-    fetch('projects.json')
-        .then(response => response.json())
-        .then(trabalho => {
-            // Get the container element
-            const container = document.getElementById('container');
+    function loadCards() {
+        fetch('../projects.json') // JSON na raiz
+            .then(res => res.json())
+            .then(trabalho => {
+                container.innerHTML = ''; // limpa os cards antigos
 
-            for (const item of trabalho) {
+                for (const item of trabalho) {
+                    const div = document.createElement('div');
+                    div.className = 'trabalho';
+                    div.innerHTML = `
+                        <img src="${item.src}?t=${new Date().getTime()}" alt="${item.name}">
+                        <div class="overlay">${item.name || ''}</div>
+                    `;
+                    // Clique no card leva para coringa.html com o id do projeto
+                    div.addEventListener('click', () => {
+                        window.location.href = `coringa.html?id=${item.id}`;
+                    });
 
-                const div = document.createElement('div');
-                div.className = 'trabalho';
-                div.innerHTML = `
-                <img src="${item.src}" alt="${item.name}">
-                <div class="overlay">${item.name || ''}</div>
-            `;
-                container.appendChild(div);
-            };
-        });
+                    container.appendChild(div);
+                }
+            })
+            .catch(err => console.error('Erro ao carregar projects.json:', err));
+    }
+
+    // Carrega inicialmente
+    loadCards();
 });
